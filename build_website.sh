02e8cb9e9052766fbb5cbf4ba9eb6f62055c3f9d
Henri Lefebvre <hri.lefebvre@gmail.com>
2022-11-14 14:21:51 +0100

    try dploying site

--- build_website.sh
+++ build_website.sh
@@ -1,8 +1,8 @@
 
-for FILE in $(find . -type f -name '*.render.Rmd')
-do
-    echo $FILE
-    R -e "rmarkdown::render(\"$FILE\")"
-done
+# for FILE in $(find . -type f -name '*.render.Rmd')
+# do
+#     echo $FILE
+#     R -e "rmarkdown::render(\"$FILE\")"
+# done
 
 jekyll build
