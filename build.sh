07f760fdd7f8bb7b04b24182c86d512d184f9751
Henri Lefebvre <hri.lefebvre@gmail.com>
2026-03-25 10:13:20 +0100

    do not build R

--- build.sh
+++ build.sh
@@ -1,11 +1,11 @@
-python3 pre-build.py
+# python3 pre-build.py
 
-FILES=$(find . -type f -name '*.rendered.Rmd' | grep -v "_site")
+# FILES=$(find . -type f -name '*.rendered.Rmd' | grep -v "_site")
 
-for FILE in $FILES
-do
-    echo $FILE
-    R --no-save --no-restore -e "rmarkdown::render(\"$FILE\")" || exit
-done
+# for FILE in $FILES
+# do
+#     echo $FILE
+#     R --no-save --no-restore -e "rmarkdown::render(\"$FILE\")" || exit
+# done
 
 jekyll build
